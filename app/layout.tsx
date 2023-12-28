34967f0abcc100221cca4c43198752484e2a4ca9
Mike Lee <mikemunkyu.lee@gmail.com>
2023-12-28 12:24:01 -0500

    add padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import { Toaster } from "@/components/ui/sonner";
+import { cn } from "@/lib/utils";
 
 import "./globals.css";
 
@@ -19,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={cn(inter.className, "p-4")}>
         {children}
         <Toaster position="bottom-center" />
       </body>
