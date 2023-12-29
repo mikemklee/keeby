229f68b162a06375f2edd1b1d0926165949f716a
Mike Lee <mikemunkyu.lee@gmail.com>
2023-12-29 15:00:53 -0500

    Cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,10 @@ import AudioSummarizer from "@/components/audio-summarizer";
 type Modes = "text" | "video" | "audio" | "pdf";
 
 export default function Home() {
-  const [mode, setMode] = useState<Modes>("audio");
+  const [mode, setMode] = useState<Modes>("text");
 
   return (
-    <div className="max-w-xl mx-auto h-full flex flex-col gap-4 justify-center">
+    <div className="max-w-xl mx-auto h-full flex flex-col gap-4 py-4">
       <div>
         <div className="space-y-1">
           <h1 className="text-5xl font-extrabold tracking-tight">tl:dr;</h1>
@@ -39,21 +39,31 @@ export default function Home() {
           >
             Video
           </div>
-          <Separator orientation="vertical" />
+          {/* <Separator orientation="vertical" />
           <div
             className={cn(mode === "pdf" ? "text-black" : "text-gray-400")}
             onClick={() => setMode("pdf")}
           >
             PDF
-          </div>
+          </div> */}
         </div>
       </div>
       <div className="min-h-96 flex flex-col gap-4">
         {mode === "text" && <TextSummarizer />}
         {mode === "audio" && <AudioSummarizer />}
         {mode === "video" && <div className="text-sm">Coming soon!</div>}
-        {mode === "pdf" && <div className="text-sm">Coming soon!</div>}
+        {/* {mode === "pdf" && <div className="text-sm">Coming soon!</div>} */}
       </div>
+      <footer className="mt-auto text-sm text-center">
+        by{" "}
+        <a
+          href="https://github.com/mikemklee"
+          className="font-semibold hover:underline"
+          target="_blank"
+        >
+          @mikemklee
+        </a>
+      </footer>
     </div>
   );
 }
